1ef28d24ae645ec083d4517f2c887d9fbf094f4c
Mark C <cookson.mark@gmail.com>
2022-07-08 22:46:29 +1200

    Complete exercise 2.17

--- Chapter02/ex2.10.cpp
+++ Chapter02/ex2.10.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 std::string global_str;
-int global_int;
 int main()
 {
-   int local_int;
-   std::string local_str;
+    double wage = 999;
+    double salary = wage;
+    std::cout << global_str;
+    int local_int;
+    std::string local_str;
+    std::cout << local_str;
+    std::cout << local_int << std::endl;
 
-    std::cout << "Global string: " << global_str << std::endl
-   << "Global int: " << global_int << std::endl
-    << "Local int: " << local_int << std::endl
-     << "Local string: " << local_str << std::endl;
-} 
+    return 0;
+}
--- Chapter02/ex2.9.cpp
+++ Chapter02/ex2.9.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
-
+std::string global_str;
 int main()
 {
-   double salary = 999.99 = wage;
+    double wage = 999;
+    double salary = wage;
+    std::cout << global_str;
+    int local_int;
+    std::cout << local_int << std::endl;
+
     return 0;
 }
